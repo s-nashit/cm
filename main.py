cd98380841d2209a1ab26ab846c7a3b9d92ccc02
Nashit Humam <85383203+s-nashit@users.noreply.github.com>
2025-10-27 19:23:59 +0530

    Refactor Streamlit app for data visualization

--- main.py
+++ main.py
@@ -1,7 +1,12 @@
 import streamlit as st
 import pandas as pd
-import pygwalker as pg
-st.title('My Application')
+from pygwalker.api.streamlit import StreamlitRenderer
+
+st.set_page_config(layout="wide")
+st.title("Data Visualization with PyGWalker")
+
 df = pd.read_csv('assignment.csv')
 
-st.write(df)
+pyg_app = StreamlitRenderer(df)
+pyg_app.explorer() 
+
